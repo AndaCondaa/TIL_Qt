d68dddbcd912105b45d1ebb180f80d6a20ce6819
AndaCondaa <simon5229@naver.com>
2022-09-14 14:06:45 +0900

    0914

---
+++ QTextBrowser/main.cpp
@@ -0,0 +1,15 @@
+#include <QApplication>
+#include <QTextBrowser>
+#include <QUrl>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QTextBrowser *tb = new QTextBrowser();
+    tb->resize(700,800);
+    tb->setSource(QUrl("index.html"));
+    tb->show();
+
+
+    return a.exec();
+}
