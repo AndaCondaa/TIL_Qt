83a3f869eaef4d24c79c1a32b60eec4ae0c0d1b5
AndaCondaa <simon5229@naver.com>
2022-12-29 17:38:13 +0900

    1229

---
+++ TestDog/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <QDebug>
+#include <QObject>
+
+class Dog : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Dog(QObject* parent = nullptr);
+
+private slots:
+    void initTestCase();
+    void init();
+    void cleanup();
+    void cleanupTestCase();
+
+    void bark();
+    void rollover();
+};
+
+#endif // DOG_H
---
+++ TestDog/main.cpp
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+#include <QCoreApplication>
+#include <QDebug>
+#include <QTest>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    Dog dog;
+    QTest::qExec(&dog);
+
+
+    return a.exec();
+}
